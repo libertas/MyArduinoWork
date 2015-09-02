3381f26edaa69c288fbf05e7055d2331c2598182
Libertas <horizonvei@gmail.com>
2015-09-02 21:39:10 +0800

    double-way output

--- sayHi/sayHi.ino
+++ sayHi/sayHi.ino
@@ -1,7 +1,8 @@
 // Detect a human and say hi to him using Attiny 13a
 #include <avr/wdt.h>
 #include <avr/sleep.h>
-int in1 = 3, out1 = 4;
+char in1 = 3, out1 = 4;
+char in2 = 1, out2 = 2;
 
 void sleep()
 {
@@ -15,18 +16,31 @@ void setup()
 {
   pinMode(in1, INPUT);
   pinMode(out1, OUTPUT);
+
+  pinMode(in2, INPUT);
+  pinMode(out2, OUTPUT);
+
   digitalWrite(out1, LOW);
+  digitalWrite(out2, LOW);
 }
 
 void loop()
 {
+  if(digitalRead(in2))
+  {
+    digitalWrite(out2, HIGH);
+    wdt_enable(WDTO_8S);
+    sleep();
+  }
+
   if(digitalRead(in1))
   {
     digitalWrite(out1, HIGH);
     wdt_enable(WDTO_8S);
     sleep();
-    
   }
+
   wdt_enable(WDTO_15MS);
   sleep();
 }
+
