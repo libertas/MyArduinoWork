d016615db3601211e0af688ef323e3ffdcdb72bc
Libertas <horizonvei@gmail.com>
2015-09-07 14:56:46 +0800

    add distance

--- car/car.ino
+++ car/car.ino
@@ -1,3 +1,4 @@
+#include <avr/wdt.h>
 #include <IRremote.h>
 
 int RECV_PIN = 9;
@@ -7,19 +8,45 @@ IRrecv irrecv(RECV_PIN);
 decode_results results;
 
 const int left1 = 5, left2 = 6, right1 = 7, right2 = 8;
-const unsigned long goForward = 0xFFC03F, goBack = 0xFF40BF, turnLeft = 0xFF708F, turnRight = 0xFF58A7;
+const unsigned long goForward = 0xFFC03F
+, goBack = 0xFF40BF
+, turnLeft = 0xFF708F
+, turnRight = 0xFF58A7;
+
+int trig = 4, echo = 3;
+
+inline double getDistance()
+{
+  digitalWrite(trig, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig, LOW);
+  return pulseIn(echo, HIGH, 2000UL) / 58.8;
+}
 
 void setup()
 {
+  wdt_enable(WDTO_250MS);
+  wdt_reset();
   Serial.begin(9600);
   irrecv.enableIRIn(); // Start the receiver
+  pinMode(left1, OUTPUT);
+  pinMode(left2, OUTPUT);
+  pinMode(right1, OUTPUT);
+  pinMode(right2, OUTPUT);
+  pinMode(echo, INPUT);
+  pinMode(trig, OUTPUT);
 }
 
 void loop()
 {
+  wdt_reset();
   unsigned long irCode = 0;
   static unsigned long lastCode;
   static unsigned char errorCount;
+  unsigned cm;
+
   if (irrecv.decode(&results))
   {
     irCode = results.value;
@@ -30,6 +57,12 @@ void loop()
   if(irCode == 0xffffffff)
   {
     irCode = lastCode;
+  }
+
+  cm = getDistance();
+  if(cm > 0.0 && cm < 5.0)
+  {
+    while(1);
   }
 
   switch(irCode)
