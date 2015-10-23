871a1c7859a70e0d1919c7049f8e88d552857ecd
libertas <horizonvei@gmail.com>
2015-10-23 14:47:16 +0800

    add timer0 and use eeprom in a new order

--- serialSwitch/serialSwitch.c
+++ serialSwitch/serialSwitch.c
@@ -1,8 +1,10 @@
 // Running on an Atmega 16a
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include <util/delay.h>
 #include <stdio.h>
+#include <string.h>
 
 void writeEEPROM(unsigned int addr, unsigned char data)
 {
@@ -50,6 +52,19 @@ void initUSART()
     UCSRC = (1 << URSEL) | (3 << UCSZ0);
 }
 
+ISR(TIMER0_OVF_vect)
+{
+    
+}
+
+void initTimer0()
+{
+    TCCR0 = (1 << CS02) | (1 << CS00);  // CLKio / 1024
+    TCNT0 = 0;
+    TIMSK = _BV(TOIE0);
+    sei();
+}
+
 #define MAXCMDLEN 100
 
 void runCmd(const unsigned char code[])
@@ -62,24 +77,24 @@ void runCmd(const unsigned char code[])
         case 'A':  // control port a
             if(code[2] - '0')
             {
-                writeEEPROM('a' + addr, '1');
+                writeEEPROM(addr, '1');
                 PORTA |= 1 << addr;
             }
             else
             {
-                writeEEPROM('a' + addr, '0');
+                writeEEPROM(addr, '0');
                 PORTA &= ~(1 << addr);
             }
             break;
         case 'C':  // control port c
             if(code[2] - '0')
             {
-                writeEEPROM('a' + 8 + addr, '1');
+                writeEEPROM(8 + addr, '1');
                 PORTC |= 1 << addr;
             }
             else
             {
-                writeEEPROM('a' + 8 + addr, '0');
+                writeEEPROM(8 + addr, '0');
                 PORTC &= ~(1 << addr);
             }
             break;
@@ -110,19 +125,20 @@ int main()
     PORTA = 0x00;
     PORTC = 0x00;
 
-    for(i = 'a'; i < 'a' + 8; i++)
+    for(i = 0; i < 8; i++)
     {
         PORTA = PORTA >> 1;
         PORTA |= ((readEEPROM(i) - '0') & 0x01) << 7;
     }
 
-    for(i = 'a' + 8; i < 'a' + 16; i++)
+    for(i = 8; i < 16; i++)
     {
         PORTC = PORTC >> 1;
         PORTC |= ((readEEPROM(i) - '0') & 0x01) << 7;
     }
 
     initUSART();
+    initTimer0();
 
     while(1)
     {
