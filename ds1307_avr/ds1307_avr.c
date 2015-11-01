cb404aee1eac4333150ba3adb09a27e34194640e
libertas <horizonvei@gmail.com>
2015-11-01 22:54:17 +0800

    add printf

--- ds1307_avr/ds1307_avr.c
+++ ds1307_avr/ds1307_avr.c
@@ -2,12 +2,24 @@
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <util/delay.h>
+#include <stdio.h>
+
+static int usart_putchar(char data, FILE *stream)
+{
+    while(!(UCSR0A & (1 << UDRE0)));
+    UDR0 = data;
+	return 0;
+}
+
+static FILE mystdout = FDEV_SETUP_STREAM(usart_putchar, NULL, _FDEV_SETUP_WRITE);
 
 void initUSART()
 {
 	UCSR0A |= _BV(U2X0);
 	UCSR0B |= _BV(RXEN0) | _BV(TXEN0) | _BV(RXCIE0);
 	UBRR0 = 12;  // 9600 at 1 MHz
+	stdout = &mystdout;
 }
 
 ISR(USART_RX_vect)
@@ -15,18 +27,6 @@ ISR(USART_RX_vect)
     unsigned char tmp;
     tmp = UDR0;
     UDR0 = tmp;
-}
-
-void sendUSART(unsigned char data)
-{
-    while(!(UCSR0A & (1 << UDRE0)));
-    UDR0 = data;
-}
-
-char receiveUSART()
-{
-    while(!(UCSR0A & (1 << RXC0)));
-    return UDR0;
 }
 
 int main()
@@ -37,7 +37,9 @@ int main()
 
 	while(1)
 	{
+		printf("Hello,World\n");
 	}
 
 	return 0;
 }
+
