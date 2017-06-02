72da5bbfca5eb5ba3cc5858627627a98e727a155
libertas <horizonvei@gmail.com>
2017-06-02 09:38:22 +0800

    update i2clcd to avoid bugs of being stuck

--- I2CLCD/I2CLCD.ino
+++ I2CLCD/I2CLCD.ino
@@ -1,32 +1,57 @@
 
 #include <LiquidCrystal.h>
+#include <Wire.h>
+
+#define I2CLCD_ADDRESS 0x12
 
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+int count = 0;
+
+char data[2][16] = {0};
 
 void setup() {
   
   lcd.begin(16, 2);
   
+  Wire.begin(I2CLCD_ADDRESS);
+  Wire.onReceive(receiveEvent);
+  
   Serial.begin(115200);
 }
 
 void loop() {
+  lcd.cursor();
+  
+  lcd.setCursor(0, 0);
+  lcd.print(data[0]);
+  lcd.setCursor(0, 1);
+  lcd.print(data[1]);
+  
+  delay(100);
   
   lcd.noCursor();
-  delay(500);
   
-  lcd.cursor();
-  delay(500);
+  lcd.setCursor(0, 0);
+  lcd.print(data[0]);
+  lcd.setCursor(0, 1);
+  lcd.print(data[1]);
+  
+  delay(100);
 }
 
 void updateLCD(char c) {
-  static int count = 0;
-  
-  lcd.setCursor(count % 16, count / 16);
-  lcd.write(c);
+  data[count / 16][count % 16] = c;
   
   count = (count + 1) % 32;
-  Serial.println(count);
+}
+
+void receiveEvent(int howMany)
+{
+  while(0 < Wire.available())
+  {
+    char c = Wire.read();
+    updateLCD(c);
+  }
 }
 
 void serialEvent() {
@@ -35,5 +60,6 @@ void serialEvent() {
     char c = (char)Serial.read(); 
     
     updateLCD(c);
+    Serial.println(count);
   }
 }
